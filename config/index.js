88805604317c0dcabb0fef881d9ceef449882ee2
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-12-14 13:47:15 -0800

    Removed port from mongo db

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/newsdb', {
+mongoose.connect('mongodb://localhost/news_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, e => console.log(e))
